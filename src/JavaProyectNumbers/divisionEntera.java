7d93aec3793d032c8d9ccb4bb971b9461e6cd79c
Noelia <noeliaestefania.castro@outlook.com>
2023-07-12 14:28:17 -0300

    Interfaz grafica de division entera

--- src/JavaProyectNumbers/divisionEntera.java
+++ src/JavaProyectNumbers/divisionEntera.java
@@ -1,24 +1,31 @@
 package JavaProyectNumbers;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class divisionEntera {
     public static void main(String arg[]){
         Scanner entrada = new Scanner(System.in);
 
-        System.out.print("-----------------------------------------------\n");
-        System.out.print("Escriba la cantidad de pelotas y luego de niños: \n");
-        System.out.print("-----------------------------------------------\n");
-        System.out.println("Pelotas: ");
-        int pelotas = entrada.nextInt();
-        System.out.println("Niños: ");
-        int niños = entrada.nextInt();
+        //System.out.print("-----------------------------------------------\n");
+        //System.out.print("Escriba la cantidad de pelotas y luego de niños: \n");
+        //System.out.print("-----------------------------------------------\n");
+        String msj = "Escriba la cantidad de pelotas y luego de niños";
+        JOptionPane.showMessageDialog(null,msj);
+        //System.out.println("Pelotas: ");
 
-        int cantXNiño = pelotas/niños;
-        int pelotasSobran = pelotas%niños;
+        String pelotas = JOptionPane.showInputDialog("Pelotas");
 
-        System.out.println("la cantidad de pelotas por niños es "+cantXNiño);
-        System.out.print("la cantidad de pelotas que sobran es "+pelotasSobran);
+        //System.out.println("Niños: ");
+        String niños = JOptionPane.showInputDialog("Niños");
+
+        int cantXNiño = Integer.parseInt(pelotas)/Integer.parseInt(niños);
+        int pelotasSobran = Integer.parseInt(pelotas)%Integer.parseInt(niños);
+
+        JOptionPane.showMessageDialog(null,"la cantidad de pelotas por niños es "+cantXNiño);
+        JOptionPane.showMessageDialog(null,"la cantidad de pelotas que sobran es "+pelotasSobran);
+        //System.out.println("la cantidad de pelotas por niños es "+cantXNiño);
+        //System.out.print("la cantidad de pelotas que sobran es "+pelotasSobran);
 
     }
 }
