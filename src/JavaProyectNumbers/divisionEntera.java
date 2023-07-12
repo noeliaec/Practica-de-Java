194aa674e7d3d6c0d430cc2794077a84e68aae26
Noelia <noeliaestefania.castro@outlook.com>
2023-07-12 14:10:46 -0300

    Agrego division entera

---
+++ src/JavaProyectNumbers/divisionEntera.java
@@ -0,0 +1,20 @@
+package JavaProyectNumbers;
+
+import java.util.Scanner;
+
+public class divisionEntera {
+    public static void main(String arg[]){
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Escriba la cantidad de pelotas y luego de niños: ");
+        int pelotas = entrada.nextInt();
+        int niños = entrada.nextInt();
+
+        int cantXNiño = pelotas/niños;
+        int pelotasSobran = pelotas%niños;
+
+        System.out.println("la cantidad de pelotas por niños es "+cantXNiño);
+        System.out.print("la cantidad de pelotas que sobran es "+pelotasSobran);
+
+    }
+}
