da2f1eeb37f14072e49a818a050202bfcc26acd9
Noelia <noeliaestefania.castro@outlook.com>
2023-08-29 14:22:55 -0300

    Agrego estructura de control Switch

--- src/JavaProjectGeometry/CalculadoraArea.java
+++ src/JavaProjectGeometry/CalculadoraArea.java
@@ -7,7 +7,7 @@ import java.text.DecimalFormat;
 public class CalculadoraArea {
     public static void main(String[] args) {
 
-        String[] opciones = {"Cuadrado", "Circulo", "Triangulo"};
+        String[] opciones = {"Cuadrado","Circulo","Triangulo","Cancelar"};
         Image icono = new ImageIcon("C:\\Users\\hades\\Downloads\\Geometry.png").getImage().getScaledInstance(50,50, Image.SCALE_SMOOTH); // Cambia la ruta por la ubicación real del icono
         ImageIcon iconoRedimensionado = new ImageIcon(icono);
 
@@ -21,39 +21,49 @@ public class CalculadoraArea {
                 opciones,
                 opciones[0]
         );
-        if (eleccion == 0) {
-            String base = (String) JOptionPane.showInputDialog(null, "Ingrese Base","Cuadrado",
-                    JOptionPane.PLAIN_MESSAGE,iconoRedimensionado, null, null);
-            String altura = (String) JOptionPane.showInputDialog(null, "Ingrese Altura","Cuadrado",
-                    JOptionPane.PLAIN_MESSAGE,iconoRedimensionado, null, null);
-            int area = Integer.parseInt(base) * Integer.parseInt(altura);
 
-            JOptionPane.showMessageDialog(null, "El área es " + area,"Cuadrado",
-                    JOptionPane.PLAIN_MESSAGE,iconoRedimensionado);
-        }
+        String base;
+        String altura;
+        double area;
 
-        if (eleccion == 1) {
-            double pi = 3.1415;
-            String radio = (String) JOptionPane.showInputDialog(null, "Ingrese Radio","Círculo",
-                    JOptionPane.PLAIN_MESSAGE,iconoRedimensionado, null, null);
-            double area = Integer.parseInt(radio) * Integer.parseInt(radio) * pi;
+        switch (eleccion){
+            case 0:
+                base = (String) JOptionPane.showInputDialog(null, "Ingrese Base","Cuadrado",JOptionPane.PLAIN_MESSAGE,iconoRedimensionado, null, null);
+                altura = (String) JOptionPane.showInputDialog(null, "Ingrese Altura","Cuadrado",
+                        JOptionPane.PLAIN_MESSAGE,iconoRedimensionado, null, null);
+                area = Double.parseDouble(base) * Double.parseDouble(altura);
 
-            DecimalFormat formato = new DecimalFormat("#.00");
-            String areaFormateada = formato.format(area);
+                JOptionPane.showMessageDialog(null, "El área es " + area,"Cuadrado",
+                        JOptionPane.PLAIN_MESSAGE,iconoRedimensionado);
+                break; //Para finalizar el programa
+            case 1:
+                double pi = 3.1415;
+                String radio = (String) JOptionPane.showInputDialog(null, "Ingrese Radio","Círculo",
+                        JOptionPane.PLAIN_MESSAGE,iconoRedimensionado, null, null);
+                area = Double.parseDouble(radio) * Double.parseDouble(radio) * pi;
 
-            JOptionPane.showMessageDialog(null, "El área es " + areaFormateada, "Círculo",
-                    JOptionPane.PLAIN_MESSAGE,iconoRedimensionado);
-        }
+                DecimalFormat formato = new DecimalFormat("#.00"); //Para redondear el valor a 2 decimales
+                String areaFormateada = formato.format(area);
 
-        if (eleccion == 2) {
-            String base = (String) JOptionPane.showInputDialog(null, "Ingrese Base","Triángulo",
-                    JOptionPane.PLAIN_MESSAGE,iconoRedimensionado, null, null);
-            String altura = (String) JOptionPane.showInputDialog(null, "Ingrese Altura","Triángulo",
-                    JOptionPane.PLAIN_MESSAGE,iconoRedimensionado, null, null);
-            double area = (double) (Integer.parseInt(base) * Integer.parseInt(altura)) / 2;
+                JOptionPane.showMessageDialog(null, "El área es " + areaFormateada, "Círculo",
+                        JOptionPane.PLAIN_MESSAGE,iconoRedimensionado);
+                break;
+            case 2:
+                base = (String) JOptionPane.showInputDialog(null, "Ingrese Base","Triángulo",
+                        JOptionPane.PLAIN_MESSAGE,iconoRedimensionado, null, null);
+                altura = (String) JOptionPane.showInputDialog(null, "Ingrese Altura","Triángulo",
+                        JOptionPane.PLAIN_MESSAGE,iconoRedimensionado, null, null);
+                area = (double) (Integer.parseInt(base) * Integer.parseInt(altura)) / 2;
 
-            JOptionPane.showMessageDialog(null, "El área es " + area,"Triángulo",
-                    JOptionPane.PLAIN_MESSAGE,iconoRedimensionado);
+                JOptionPane.showMessageDialog(null, "El área es " + area,"Triángulo",
+                        JOptionPane.PLAIN_MESSAGE,iconoRedimensionado);
+                break;
+            case 3:
+                System.exit(0);
+
+
+
+
         }
     }
 }
